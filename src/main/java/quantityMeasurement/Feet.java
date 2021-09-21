4f60e1b8ac07ea5cc94516af8556e5110e07cddb
dipalimagare <dipalimagare2026@gmail.com>
2021-09-21 22:25:45 +0530

    added perform test for equality

---
+++ src/main/java/quantityMeasurement/Feet.java
@@ -0,0 +1,22 @@
+package quantityMeasurement;
+
+public class Feet {
+    private final double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Feet other = (Feet) obj;
+        if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+            return false;
+        return true;
+    }
+}
---
+++ src/main/java/quantityMeasurement/QuantityTest.java
@@ -0,0 +1,75 @@
+package quantityMeasurement;
+import org.junit.*;
+
+public class QuantityTest {
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotSame(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetandNullFeet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotSame(feet1, feet2);
+    }
+
+    @Test
+    public void givenFeetandFeetFromSameRef_WhenEqual_ShouldReturnTrue() {
+        Feet feet1 = new Feet(0);
+        boolean result = feet1 == feet1;
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given0Feetand0FeetFromDifferentType_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0);
+        Assert.assertNotSame(feet1, feet2);
+    }
+
+    @Test
+    public void given0Inchand0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    public void given0Inchand1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotSame(inch1, inch2);
+    }
+
+    @Test
+    public void given0InchandNullInch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = null;
+        Assert.assertNotSame(inch1, inch2);
+    }
+
+    @Test
+    public void givenInchandInchFromSameRef_WhenEqual_ShouldReturnTrue() {
+        Inch inch1 = new Inch(0);
+        boolean result = inch1 == inch1;
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given0Inchand0InchFromDifferentType_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0);
+        Assert.assertNotSame(inch1, inch2);
+    }
+
+}
